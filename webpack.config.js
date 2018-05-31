02aee23032162b39ca765c3c2e73754dd04504ac
ukyo <ukyo.web@gmail.com>
2018-05-31 18:57:51 +0900

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader"
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
+  }
+}
